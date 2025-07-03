c481880a082964b97bf13906e7d14b6b6ec5580e
khernan14 <kevd_hernandezm@unicah.edu>
2025-07-03 17:19:21 -0600

    quitando un console.log

--- src/config/variables.jsx
+++ src/config/variables.jsx
@@ -1,6 +1,5 @@
 // src/js/config/api.js
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-console.log("API_BASE_URL", API_BASE_URL);
 
 export const endpoints = {
   login: `${API_BASE_URL}/auth/login`,
